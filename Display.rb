84847334a0a16532ac467587b4c44e781bed2b70
TheSlyPig <kcunningham04@hotmail.com>
2018-01-16 10:42:04 -0500

    Refactor long methods

--- Display.rb
+++ Display.rb
@@ -15,28 +15,30 @@ class Display
   end
 
   def get_square_color(pos, i, j)
-    if i % 2 == 0
-      if j % 2 != 0
-        square_color = :light_black
-      end
-    elsif j % 2 == 0
-      square_color = :light_black
-    end
-
-    if @cursor.selected && @stored_moves.nil?
-      @stored_moves = @board[@selected_pos].valid_moves
-    end
+    square_color = get_default_color(pos, i, j)
+    @stored_moves = @board[@selected_pos].valid_moves if @cursor.selected && @stored_moves.nil?
 
     unless @stored_moves.nil?
       square_color = :green if @stored_moves.include?(pos)
     end
 
     if @cursor.cursor_pos == pos
-      if @cursor.selected
-        square_color = :red
+      @cursor.selected ? square_color = :red : square_color = :magenta
+    end
+    return square_color
+  end
+
+  def get_default_color(pos, i, j)
+    if i % 2 == 0
+      if j % 2 != 0
+        square_color = :light_black
       else
-        square_color = :light_blue
+        square_color = :blue
       end
+    elsif j % 2 == 0
+      square_color = :light_black
+    else
+      square_color = :blue
     end
     return square_color
   end
@@ -48,9 +50,10 @@ class Display
       (0..7).each do |j|
         pos = [i, j]
         piece_string = @board[pos].to_s
-
         square_color = get_square_color(pos, i, j)
-        row_image << " ".colorize( :background => square_color) + piece_string.colorize( :background => square_color) + " ".colorize( :background => square_color)
+        row_image << " ".colorize( :background => square_color) +
+          piece_string.colorize( :background => square_color) +
+          " ".colorize( :background => square_color)
       end
       image << row_image
     end
