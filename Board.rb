6da44f1dd4114c03f3c4308fb9822f599de5dc33
Kegan Cunningham <kcunningham04@hotmail.com>
2018-01-16 08:51:24 -0500

    Remove unnecessary comments

--- Board.rb
+++ Board.rb
@@ -5,42 +5,40 @@ class PieceNotFoundError < ChessError; end
 class InvalidMoveError < ChessError; end
 
 class Board
-  # attr_reader :grid
-  
-  
+
   def make_special_row(color)
     top_bot = color == :white ? 0 : 7
     [
-      Rook.new(self, [top_bot, 0], color), 
-      Knight.new(self, [top_bot, 1], color), 
-      Bishop.new(self, [top_bot, 2], color), 
-      Queen.new(self, [top_bot, 3], color), 
-      King.new(self, [top_bot, 4], color), 
-      Bishop.new(self, [top_bot, 5], color), 
-      Knight.new(self, [top_bot, 6], color), 
+      Rook.new(self, [top_bot, 0], color),
+      Knight.new(self, [top_bot, 1], color),
+      Bishop.new(self, [top_bot, 2], color),
+      Queen.new(self, [top_bot, 3], color),
+      King.new(self, [top_bot, 4], color),
+      Bishop.new(self, [top_bot, 5], color),
+      Knight.new(self, [top_bot, 6], color),
       Rook.new(self, [top_bot, 7], color)
     ]
   end
 
-  
+
   def make_pawn_row(color)
     top_bot = color == :white ? 1 : 6
     [
-      Pawn.new(self, [top_bot, 0], color), 
-      Pawn.new(self, [top_bot, 1], color), 
-      Pawn.new(self, [top_bot, 2], color), 
-      Pawn.new(self, [top_bot, 3], color), 
-      Pawn.new(self, [top_bot, 4], color), 
-      Pawn.new(self, [top_bot, 5], color), 
-      Pawn.new(self, [top_bot, 6], color), 
+      Pawn.new(self, [top_bot, 0], color),
+      Pawn.new(self, [top_bot, 1], color),
+      Pawn.new(self, [top_bot, 2], color),
+      Pawn.new(self, [top_bot, 3], color),
+      Pawn.new(self, [top_bot, 4], color),
+      Pawn.new(self, [top_bot, 5], color),
+      Pawn.new(self, [top_bot, 6], color),
       Pawn.new(self, [top_bot, 7], color)
     ]
   end
-  
+
   def initialize
-    
+
     nil_row = Array.new(8) { NullPiece.instance }
-    
+
     @grid = [
       self.make_special_row(:white),
       self.make_pawn_row(:white),
@@ -52,7 +50,7 @@ class Board
       self.make_special_row(:black)
     ]
   end
-  
+
   def move_piece(start_pos, end_pos)
     piece_to_move = self[start_pos]
     if piece_to_move.is_a?(NullPiece)
@@ -63,11 +61,11 @@ class Board
     piece_to_move.pos = end_pos
     self[end_pos] = piece_to_move
     self[start_pos] = NullPiece.instance
-    
+
     piece_to_move.has_moved if piece_to_move.is_a?(Pawn)
-    
+
   end
-  
+
   def force_move_piece(start_pos, end_pos)
     piece_to_move = self[start_pos]
     piece_to_move.pos = end_pos
@@ -75,36 +73,36 @@ class Board
     self[start_pos] = NullPiece.instance
   end
 
-  
+
   def deep_dup
     board_dup = Board.new
-    
-    dup_grid = @grid.map do |row| 
+
+    dup_grid = @grid.map do |row|
       row.map do |piece|
         piece.dup_with_new_board(board_dup)
       end
     end
-    
+
     board_dup.grid = dup_grid
-    
-    board_dup    
+
+    board_dup
   end
-  
+
   def [](pos)
     x, y = pos
     @grid[x][y]
   end
-  
+
   def []=(pos, value)
     x, y = pos
     @grid[x][y] = value
   end
-  
+
   def self.in_bounds?(pos)
     x, y = pos
     x.between?(0, 7) && y.between?(0, 7)
   end
-  
+
   def in_check?(color)
     kings = []
     @grid.each do |row|
@@ -112,9 +110,9 @@ class Board
         kings << piece if piece.is_a?(King)
       end
     end
-    
+
     king_pos = kings.select { |king| king.color == color }.first.pos
-    
+
     enemy_moves = []
     @grid.each do |row|
       row.each do |piece|
@@ -123,14 +121,14 @@ class Board
         end
       end
     end
-    
-    enemy_moves.include?(king_pos)    
+
+    enemy_moves.include?(king_pos)
   end
-  
+
   def checkmate?(color)
     return false unless in_check?(color)
     moves = []
-    
+
     @grid.each do |row|
       row.each do |piece|
         if piece.color == color
@@ -138,16 +136,16 @@ class Board
         end
       end
     end
-    
+
     moves.empty?
   end
-  
-  
+
+
   #for debugging only
   def put_piece(piece)
     self[piece.pos] = piece
   end
-  
+
   protected
   attr_writer :grid
 end
