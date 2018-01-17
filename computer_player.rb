107dc2a7132c6703d412c280f4cf49d3f621a9d4
TheSlyPig <kcunningham04@hotmail.com>
2018-01-16 22:04:11 -0500

    Add gitignore and screenshot

--- computer_player.rb
+++ computer_player.rb
@@ -15,7 +15,7 @@ class ComputerPlayer
   def play_turn
     loop do
       handle_rendering
-      sleep(0.20)
+      sleep(0.50)
       forfeit = "forfeit"
       tries = 0
       while forfeit == "forfeit" && tries < 10
